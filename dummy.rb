51c61bcc2602b1b68c0539beebdbc4dacdbcf50b
Simone <msrondys@gmail.com>
2013-12-02 00:35:47 -0800

    new web app pretty much done

--- dummy.rb
+++ dummy.rb
@@ -8,15 +8,16 @@ class MySite < Sinatra::Base
     @posts = Dir.glob("views/posts/*.erb").map do |post_name|
       post_name.split("/").last.slice(0..-5)
     end
+    @sorted_posts = meta_data.sort_by {|post, date_hash| date_hash["date"]}.reverse
   end
 
   get "/" do
-    erb :index
+    erb :home
   end
 
-  # get "/blog" do
-  #   erb :layout
-  # end
+  get "/blog" do
+    erb :blog
+  end
 
   get "/blog/:post_name" do
     page = erb("/posts/#{params[:post_name]}".to_sym, layout: false) 
@@ -24,16 +25,16 @@ class MySite < Sinatra::Base
     erb page_body
   end
 
-def meta_data  
-  if @meta_data
-    @meta_data
-  else
-    @meta_data = {} 
-    @posts.each do |post|
-      html = erb("/posts/#{post}".to_sym, layout: false)  
-      meta = YAML.load(html.split("\n\n", 2).first)
-      @meta_data[post] = meta
-    end 
+  def meta_data  
+    if @meta_data
+      @meta_data
+    else
+      @meta_data = {} 
+      @posts.each do |post|
+        html = erb("/posts/#{post}".to_sym, layout: false)  
+        meta = YAML.load(html.split("\n\n", 2).first)
+        @meta_data[post] = meta
+      end 
     @meta_data
     end
   end
